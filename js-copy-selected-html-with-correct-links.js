8f5b6b7353a15174f80fdd5d4fcac661a06a7e5b
dev-olga <kulnevao@gmail.com>
2021-09-08 11:16:24 +0300

    Create js-copy-selected-html-with-correct-links.js

---
+++ js-copy-selected-html-with-correct-links.js
@@ -0,0 +1,15 @@
+ $('#txt').bind('copy', function (event) {
+                
+     var clipboardData = event.clipboardData || window.clipboardData || event.originalEvent.clipboardData;
+     
+     var range = window.getSelection().getRangeAt(0);
+		 var selectionContents = range.cloneContents();	
+     //var div = document.getElementById("output");
+     var div = document.createElement("div");
+		 div.appendChild(selectionContents);
+     
+     clipboardData.setData("text/plain",div.innerHTML);
+
+     event.preventDefault();
+     
+  });       
